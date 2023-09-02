ea31b45cd557ff76204a8f1026ba27a30f6631ce
Sayantan Sarkar <sayantans643@gmail.com>
2023-09-03 01:47:18 +0530

    Time: 1 ms, Memory: 39.8 MB - LeetHub

--- pascals-triangle-ii/pascals-triangle-ii.java
+++ pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,21 +1,14 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        return helper(new ArrayList<>(),rowIndex, 0);
+        Integer[] arr = new Integer[rowIndex +1];
+        Arrays.fill(arr,0);
+        arr[0] =1;
+        for(int i=0; i<=rowIndex;i++){
+            for(int j=i;j>=1;j--) {
+                arr[j] += arr[j-1];
+            }
+        }
+        return Arrays.asList(arr);
     }
 
-    public List<Integer> helper(List<Integer> currList, int rowIndex, int currRow) {
-        if(currRow > rowIndex)
-            return currList;
-        if(currRow == 0 || currRow == 1)
-            currList.add(1);
-        ArrayList<Integer> prevList = new ArrayList<>();
-        for(int j: currList)
-            prevList.add(j);
-        for(int i=1; i<currRow; i++) {
-            currList.set(i,prevList.get(i-1) + prevList.get(i));
-            if(i+1 == currList.size())
-                currList.add(1);
-        }
-        return helper(currList, rowIndex, currRow +1);
-    }
 }
